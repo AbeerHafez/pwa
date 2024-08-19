ac27133ee4ea71252109f05b315a589380072614
Salsabeel <58127260+sals1995@users.noreply.github.com>
2024-08-19 11:49:03 +0300

    getting started angular 18

--- components/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {  TodosComponent } from './todos/todos.component';
-
-export const routes: Routes = [
-    { path: '',  component: DashboardComponent },
-    { path: 'home', component: DashboardComponent },
-    { path: 'todos', component: TodosComponent }
-  ];
